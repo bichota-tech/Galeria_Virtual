364762c279335671c29a8939d0e26bccf9f1e4a9
Adalid Martinez <adalvarez2021@gmail.com>
2025-09-03 15:31:31 +0200

    terminado el header y la seccion de inicio desktop

--- js/script.js
+++ js/script.js
@@ -1,23 +1,31 @@
-document.getElementById("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", ()=> {
+    const mycarousel = document.querySelector("#mycarousel");
     
+    //inicializar carrusel
+    const carousel = new 
+    bootstrap.Carousel(mycarousel,{ 
+      interval:3000, //tiempo entre slides (ms) (0 = no auto)
+      ride: "carousel", //para que arranque solo
+      pause: "hover", //se detiene al pasar el mouse
+      wrap: true // vuelve al inicio tras el ultimo slide
+    });
+
+    //Botones
+
+    const prevBtn = document.getElementById("prevBtn");
+    const nextBtn = document.getElementById("nextBtn");
+    
+    prevBtn.addEventListener("click", (e)=>{
+      e.preventDefault(); //evita el salto de pagina
+      carousel.prev();
+    })
+
+    nextBtn.addEventListener("click", (e) =>{
+      e.preventDefault();
+      carousel.next();
+    });
 })
-const swiper = new Swiper('.swiper', {
-    loop: true,              // carrusel infinito
-    autoplay: {
-      delay: 3000,           // cambia cada 3s
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,       // bullets clicables
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    effect: 'slide',         // puedes probar 'fade', 'cube', 'coverflow'
-    speed: 800,              // transición más fluida
-  });
+
 
 
  
